c474982450ed252dc428feb849454db9d2f30a03
nicoo <nicoo@mur.at>
2021-05-22 14:24:21 +0200

    Simplify interface for tree.mem
    Also reduces the number of times `memory.current` is read.

--- memtree.py
+++ memtree.py
@@ -27,8 +27,11 @@ def total_memory() -> MemoryAmount:
     return MemoryAmount(sysconf('SC_PAGE_SIZE') * sysconf('SC_PHYS_PAGES'))
 
 def tree(p: Path = Path('/sys/fs/cgroup/')) -> Tree:
-    def mem(q: Path) -> MemoryAmount:
-        return MemoryAmount((q / 'memory.current').read_text())
+    def mem(q: Path) -> Optional[MemoryAmount]:
+        try:
+           return MemoryAmount((q / 'memory.current').read_text())
+        except FileNotFoundError:
+            return None
 
     def name(q: Path) -> str:
         if '.' in q.name:
@@ -39,11 +42,11 @@ def tree(p: Path = Path('/sys/fs/cgroup/')) -> Tree:
         return q.name
 
     def _tree(p: Path) -> Tree:
-        # TODO: Avoid reading `q/memory.current` 3 times for each cgroup  >_>'
-        if (p / 'memory.current').exists():
-            t = Tree(f"{name(p)}: {mem(p)} ({100 * mem(p) / total :.0f}%)")
-        else:
+        m = mem(p)
+        if m is None:
             t = Tree(f"{name(p)}")
+        else:
+            t = Tree(f"{name(p)}: {m} ({100 * m / total :.0f}%)")
 
         children = sorted(
             ( q for q in p.iterdir() if q.is_dir() and mem(q) != 0 ),
