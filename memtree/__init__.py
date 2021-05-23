b1d9420193bb61750859506da20d66eabe723c5d
nicoo <nicoo@mur.at>
2021-05-24 00:56:18 +0200

    Reformat to comply with PEP8

--- memtree/__init__.py
+++ memtree/__init__.py
@@ -9,37 +9,38 @@ from rich.tree import Tree
 
 from .colors import default_palette
 
-assert system() == 'Linux', f"{__name__} only works on Linux."
+assert system() == "Linux", f"{__name__} only works on Linux."
 
-_STRIPPED_EXTS = frozenset((
-    "scope", "service", "slice"
-))
+_STRIPPED_EXTS = frozenset(("scope", "service", "slice"))
+
 
 class MemoryAmount(int):
     def __str__(self):
-        for p in ('', 'ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'):
+        for p in ("", "ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi", "Yi"):
             if self < 1024:
                 break
             self /= 1024
 
         return f"{self:.0f} {p}B"
 
-def tree(p: Path = Path('/sys/fs/cgroup/'), *, color: Optional[Callable[[float], str]] = None) -> Tree:
+
+def tree(p: Path = Path("/sys/fs/cgroup/"), *,
+         color: Optional[Callable[[float], str]] = None) -> Tree:
     if color is None:
         color = default_palette()
 
     def mem(q: Path) -> Optional[MemoryAmount]:
         try:
-            return MemoryAmount((q / 'memory.current').read_text())
+            return MemoryAmount((q / "memory.current").read_text())
         except FileNotFoundError:
             return None
 
     def name(q: Path) -> str:
-        if '.' in q.name:
-            prefix, ext = q.name.rsplit(sep='.', maxsplit=1)
+        if "." in q.name:
+            prefix, ext = q.name.rsplit(sep=".", maxsplit=1)
             if ext in _STRIPPED_EXTS:
                 return prefix
-            
+
         return q.name
 
     def _tree(p: Path) -> Tree:
@@ -47,11 +48,15 @@ def tree(p: Path = Path('/sys/fs/cgroup/'), *, color: Optional[Callable[[float],
         if m is None:
             t = Tree(f"{name(p)}")
         else:
-            t = Tree(f"{name(p)}: {m} ({100 * m/vm.total :.0f}%)", style=color(m / vm.used))
+            t = Tree(
+                f"{name(p)}: {m} ({100 * m/vm.total :.0f}%)",
+                style=color(m / vm.used)
+            )
 
         children = sorted(
-            ( q for q in p.iterdir() if q.is_dir() and mem(q) != 0 ),
-            key = mem, reverse = True
+            (q for q in p.iterdir() if q.is_dir() and mem(q) != 0),
+            key=mem,
+            reverse=True,
         )
         for q in children:
             t.add(_tree(q))
--- memtree/cli.py
+++ memtree/cli.py
@@ -1,8 +1,10 @@
 from rich import print
 from . import tree
 
+
 def main():
     print(tree())
+
 
 if __name__ == "__main__":
     main()
