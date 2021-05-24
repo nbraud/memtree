8f28de164e4aae9b7a1bd532e6183f17f892200f
nicoo <nicoo@mur.at>
2021-05-24 19:38:43 +0200

    MemoryAmount: Add new test for very-large values

--- tests/test_amount.py
+++ tests/test_amount.py
@@ -13,3 +13,13 @@ def test_any_value(multiplier: int, prefix: str, i: int, f: float):
     n = MemoryAmount(multiplier * i + int(f * multiplier))
     note("n = {n!r} B")
     assert str(n) == f"{i + 1 if f > 0.5 and multiplier > 1 else i} {prefix}B"
+
+
+MAX_PREFIX = MemoryAmount.IEC_PREFIXES[-1]
+MAX_MULTIPLIER = 1024 ** (len(MemoryAmount.IEC_PREFIXES) - 1)
+
+
+@given(i=st.integers(1), j=st.integers(0, MAX_MULTIPLIER - 1))
+def test_largest_values(i: int, j: int):
+    n = MemoryAmount(MAX_MULTIPLIER * i + j)
+    assert str(n) == f"{i if j < MAX_MULTIPLIER / 2 else i + 1} {MAX_PREFIX}B"
