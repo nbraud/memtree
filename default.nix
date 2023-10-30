054c880b7334d5cddb043bf5558ccde36135c127
nicoo <nicoo@mur.at>
2023-10-30 14:49:28 +0000

    gitignore nix-build's symlink

--- default.nix
+++ default.nix
@@ -3,12 +3,25 @@ with pkgs;
 
 let
 	py = python3;
-	drv = callPackage ./package.nix { python3Packages = py.pkgs; };
+	python3Packages = py.pkgs;
+
+	drv = callPackage ./package.nix { inherit python3Packages; };
+	extraDependencies = callPackage ./extra-dependencies.nix { inherit python3Packages; };
+
+	devTools = with lib; pipe ./pyproject.toml [
+		readFile
+		builtins.fromTOML
+		(attrByPath [ "tool" "poetry" "dev-dependencies" ] {})
+		attrNames
+		(names: attrVals names (extraDependencies // python3Packages))
+	];
+
 in
 mkShell {
 	nativeBuildInputs = [
 		drv
 		poetry
+		python3Packages.poetry-core
 		py
-	];
+	] ++ devTools;
 }
