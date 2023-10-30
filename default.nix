d3e8cfbb8ab70f3d90a6f58ff57b445719e8308d
nicoo <nicoo@mur.at>
2023-10-30 14:48:11 +0000

    Use fully nix-managed environment

--- default.nix
+++ default.nix
@@ -1,15 +1,14 @@
 { pkgs ? import <nixpkgs> {}}:
 with pkgs;
 
-let py = python3;
+let
+	py = python3;
+	drv = callPackage ./package.nix { python3Packages = py.pkgs; };
 in
 mkShell {
-  nativeBuildInputs = [
-    poetry
-    py
-  ] ++ (with py.pkgs; [
-    ipython
-    psutil
-    rich
-  ]);
+	nativeBuildInputs = [
+		drv
+		poetry
+		py
+	];
 }
