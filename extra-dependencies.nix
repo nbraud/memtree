3183bd904ba50a2001e65cae5d939d687f7e8931
nicoo <nicoo@mur.at>
2023-10-31 17:03:32 +0000

    Configure flake8 via pyproject.toml

--- extra-dependencies.nix
+++ extra-dependencies.nix
@@ -45,4 +45,24 @@ in
 
 		doCheck = false;  # TODO: fix test run
 	};
+
+	flake8-pyproject = buildPythonPackage rec {
+		pname = "flake8-pyproject";
+		version = "1.2.3";
+		pyproject = true;
+
+		src = fetchFromGitHub {
+			owner = "john-hen";
+			repo  = "Flake8-pyproject";
+			rev   = version;
+			hash  = "sha256-bPRIj7tYmm6I9eo1ZjiibmpVmGcHctZSuTvnKX+raPg=";
+		};
+
+		nativeBuildInputs = with python3Packages; [
+			flit-core
+		];
+		propagatedBuildInputs = with python3Packages; [
+			flake8
+		];
+	};
 }
