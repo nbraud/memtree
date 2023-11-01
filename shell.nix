nbraud/memtree

commit 57a15a5e19b6797b8e3515292b10ed3eb6020398

shell.nix: Do not include drv
Otherwise, the env gets rebuilt on every code change

shell.nix
@@ -6,15 +6,9 @@ let
 	};
 in
 { pkgs ? import nixpkgs { }}:
-with pkgs;
 
-let
-	ciEnv = import ./ci.nix { inherit pkgs; };
-	drv = callPackage ./package.nix { };
-in
-ciEnv.override (self: {
-	nativeBuildInputs = self.nativeBuildInputs ++ [
-		drv
-		poetry
+(import ./ci.nix {}).override (self: {
+	nativeBuildInputs = with pkgs; self.nativeBuildInputs ++ [
+		pkgs.poetry
 	];
 })

commit 0db6ba76ca9f14e587fa6fd278d0ad4c74fc7f22

shell.nix: Pass `pkgs` to ci.nix in case it was overriden
Bug introduced in 57a15a5e19b6797b8e3515292b10ed3eb6020398

shell.nix
@@ -7,7 +7,7 @@ let
 in
 { pkgs ? import nixpkgs { }}:
 
-(import ./ci.nix {}).override (self: {
+(import ./ci.nix { inherit pkgs; }).override (self: {
 	nativeBuildInputs = with pkgs; self.nativeBuildInputs ++ [
 		pkgs.poetry
 	];