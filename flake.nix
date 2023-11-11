8d36dae72242d4c9c6b1f6b17bfd663962d572be
nicoo <nicoo@mur.at>
2023-11-11 09:41:21 +0000

    flake.nix: Mild refactor
    Also fixed indentation while I was at it.  =^.^=

--- flake.nix
+++ flake.nix
@@ -1,23 +1,20 @@
 {
-  description = "Dev environment for `memtree`";
+	description = "Dev environment for `memtree`";
 
-  inputs = {
-    # Actual versions are pinned in lockfile
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
+	inputs = {
+		# Actual versions are pinned in lockfile
+		flake-utils.url = "github:numtide/flake-utils";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+	};
 
-  outputs = { self, flake-utils, nixpkgs }:
-    # TODO: Figure out the whole nix-systems/linux thing
-    flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system: {
-      packages.default =
-        with import nixpkgs { inherit system; };
-        (callPackage ./package.nix {}).memtree;
+	outputs = { self, flake-utils, nixpkgs }:
+		# TODO: Figure out the whole nix-systems/linux thing
+		flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system:
+			let pkgs = import nixpkgs { inherit system; };
+			in {
+				packages.default = (pkgs.callPackage ./package.nix {}).memtree;
 
-      packages.ci =
-        import ./ci.nix { pkgs = nixpkgs.legacyPackages.${system}; };
-
-      devShells.default =
-        import ./shell.nix { pkgs = nixpkgs.legacyPackages.${system}; };
-    });
+				devShells.ci      = import ./ci.nix    { inherit pkgs; };
+				devShells.default = import ./shell.nix { inherit pkgs; };
+			});
 }
