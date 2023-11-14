4323f5857c53c1c1a02c27e20144dabf539da60c
nicoo <nicoo@mur.at>
2023-11-14 16:23:13 +0000

    Update for new `bork` version, now packaged in `nixpkgs`  🎉

--- extra-dependencies.nix
+++ extra-dependencies.nix
@@ -5,30 +5,6 @@ let
 in
 
 {
-	bork = buildPythonPackage rec {
-		pname = "bork";
-		version = "6.0.1";
-		pyproject = true;
-
-		propagatedBuildInputs = with python3Packages; [
-			wheel
-			build
-			packaging
-			pep517
-			toml
-			twine
-			click
-			coloredlogs
-		];
-
-		src = fetchFromGitHub {
-			owner = "duckinator";
-			repo = pname;
-			rev = "v${version}";
-			hash = "sha256-/NjO8XdmL0jsdrZvJzXmW2K/nWw/ukP5FDHinQZ+sdE=";
-		};
-	};
-
 	flake8-commas = buildPythonPackage rec {
 		pname = "flake8-commas";
 		version = "2.1.0";
