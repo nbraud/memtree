98cee1838b6848ac027fed955284bfaf5de944e6
nicoo <nicoo@mur.at>
2023-11-16 18:28:20 +0000

    memtree.colors.turbo_data: Validate during loading

--- memtree/colors.py
+++ memtree/colors.py
@@ -1,18 +1,29 @@
 import json
 from functools import cache
 from importlib import resources
-from typing import Callable, Sequence, Tuple, TypeVar
+from typing import Any, Callable, Sequence, Tuple, TypeVar
 from warnings import warn
 
 
-@cache
-def turbo_data() -> Sequence[Tuple[float, float, float]]:
-    with resources.files(__package__).joinpath("data/turbo.json").open() as f:
-        return tuple(map(tuple, json.load(f)))
-
-
-T = TypeVar("T")
+T = TypeVar('T', str, Sequence[str])
 Palette = Callable[[float], str]
+Vector = Tuple[float, float, float]
+
+
+@cache
+def turbo_data() -> Sequence[Vector]:
+    def vector(v: Any) -> Vector:
+        if not isinstance(v, Sequence):
+            raise TypeError("Expected a sequence", v)
+        if not len(v) == 3:
+            raise ValueError("Expected a 3-elements sequence")
+        x, y, z = v
+        if not all(isinstance(t, float) for t in (x, y, z)):
+            raise TypeError("Expected floats")
+        return (x, y, z)
+
+    with resources.files(__package__).joinpath("data/turbo.json").open() as f:
+        return tuple(vector(v) for v in json.load(f))
 
 
 def clip(f: Callable[[float], T]) -> Callable[[float], T]:
